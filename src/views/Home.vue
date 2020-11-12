2673d25b0cad83b08eaeec8a9e5e28221b29e82f
user-guyue <guyueximei@163.com>
2020-11-12 23:43:53 +0800

    11-12

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2020-11-11 21:41:57
- * @LastEditTime: 2020-11-11 21:52:19
+ * @LastEditTime: 2020-11-12 23:42:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_ui\src\views\Home.vue
 -->
 <template>
   <div id="app">
-    <div>9999</div>
+    <div>this a Home page</div>
   </div>
 </template>
 
