3e84bf1db7163c9d7ac6483cb7dea53521d67ef3
user-guyue <guyueximei@163.com>
2020-12-03 22:51:52 +0800

    12-3 first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <!--
  * @Author: your name
  * @Date: 2020-11-11 21:41:56
- * @LastEditTime: 2020-11-20 00:08:34
+ * @LastEditTime: 2020-12-03 21:41:25
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_ui\src\App.vue
 -->
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <a-config-provider :locale="locale">
+    <div id="app">
+      <router-view/>
+    </div>
+  </a-config-provider>
 </template>
-
+<script>
+  import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
+  export default {
+    data() {
+      return {
+        locale: zhCN,
+      };
+    },
+  };
+</script>
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
