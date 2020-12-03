f178d399a5aaa8f77355be03b809a0d4a0d3980e
user-guyue <guyueximei@163.com>
2020-12-04 02:06:17 +0800

    12-4 first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-11 21:41:57
- * @LastEditTime: 2020-12-03 22:14:09
+ * @LastEditTime: 2020-12-03 23:47:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \响应式布局d:\practise\projectVue\vue-web_ui\src\router\index.js
@@ -32,6 +32,21 @@ const routes = [
     path: '/about',// 访问的地址
     name: 'About',
     component: () => import('@/views/About')// 页面的路径
+  },
+  {
+    path: '/403',// 访问的地址
+    name: '403',
+    component: () => import('@/views/exception/403')// 页面的路径
+  },
+  {
+    path: '/404',// 访问的地址
+    name: '404',
+    component: () => import('@/views/exception/404')// 页面的路径
+  },
+  {
+    path: '/500',// 访问的地址
+    name: '500',
+    component: () => import('@/views/exception/500')// 页面的路径
   }
 ]
 const router = new VueRouter({
